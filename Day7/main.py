50f5e227cddcd5ea6bc926b1f9887cb67fccd5c3
Sasmitha Karthikeyan <sasmithakarthikeyan@gmail.com>
2026-08-25 18:17:42 +0530

    Completed day 7 modules packages and virtual environment

---
+++ Day7/main.py
@@ -0,0 +1,28 @@
+#task 1
+'''import math_tools
+
+print(math_tools.add(2,3))
+print(math_tools.multiply(2,3))
+
+print(math_tools.__file__)
+print(dir(math_tools))'''
+
+#task 2
+'''from math_tools import add
+print(add(4,5))'''
+
+#task 3
+'''from tools.calculator import add
+
+print(add(10, 20))'''
+
+#task 4
+'''import random
+print(random.randint(1,100))'''
+
+#challenge
+import calculator
+print(calculator.add(3,5))
+print(calculator.subtract(7,2))
+print(calculator.multiply(3,2))
+print(calculator.divide(9,3))
---
+++ Day7/tools/converter.py
@@ -0,0 +1,5 @@
+def km_to_miles(km):
+    return km * 0.621371
+
+def miles_to_km(miles):
+    return miles / 0.621371
